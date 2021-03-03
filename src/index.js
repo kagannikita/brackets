451916b7f28308e94f574651bd17c268ac1d695d
kagannikita <kagan.nikita@bk.ru>
2021-03-03 16:40:15 +0600

    feat:write function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let arr=[];
+
+    [...str].map((first,i,str)=>{
+        bracketsConfig.map((second,j,bracketsConfig)=>{
+            if(str[i]===bracketsConfig[j][1]&&arr[arr.length-1]===bracketsConfig[j][0]){
+                arr.pop();
+            }
+            else if(str[i]===bracketsConfig[j][0]){
+                arr.push(str[i]);
+            }
+        });
+    });
+    return str.length % 2 === 0 && arr.length===0;
 }
